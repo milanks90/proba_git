67eb33a5d7c8b44d24d5b0805afb6107a6f4a773
Tehnicom-Milan <Tehnicom-Milan@Tehnicom-Milan>
2018-03-24 10:59:45 +0100

    prvi komit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        
+        <meta charset="utf-8">
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+        <meta name="keywords" content="Tehnicom Computers And Communications">
+        <meta name="description" content="Programiranje, Servisiranje, Fiskalne kase, Video nadzor">
+        
+        <title>GIT</title>
+        
+    </head>
+
+    <body>
+        
+        
+        
+    </body>
+</html>
